d5268f2e908e54fb3b253a46e6f5cb036b217582
romankuramshin <rkuramshin@isgneuro.com>
2023-07-10 14:19:58 -0700

    added endpoint for static files upload

--- ot_simple_rest/handlers/eva/file_load.py
+++ ot_simple_rest/handlers/eva/file_load.py
@@ -7,34 +7,35 @@ import tornado.web
 import tornado.httputil
 
 from handlers.eva.base import BaseHandler
-from tools.svg_manager import SVGManager
+from tools.file_manager import FileManager
 
-
-class SvgLoadHandler(BaseHandler):
-
-    MAX_FILE_SIZE = 1024
+class FileLoadHandler(BaseHandler):
 
     def initialize(self, **kwargs):
         super().initialize(kwargs['db_conn_pool'])
         self.file_conf = kwargs['file_upload_conf']
         self.static_conf = kwargs['static_conf']
         self.logger = logging.getLogger('osr')
-        svg_path = self.file_conf.get('svg_path', os.path.join(self.static_conf['static_path'], 'svg'))
-        self.svg_manager = SVGManager(svg_path)
+
+        self.MAX_FILE_SIZE = int(self.file_conf.get('max_file_size')) or 10240
+        self.file_path = self.file_conf.get('path', self.static_conf['static_path'])
+        
+        self.file_manager = FileManager(self.file_path)
 
     async def post(self):
         try:
+            print(123321)
             body = self.request.body
             args, files = {}, {}
             tornado.httputil.parse_body_arguments(self.request.headers['Content-Type'], body, args, files)
             _file = files['file'][0]
             if sys.getsizeof(_file) > self.MAX_FILE_SIZE:
-                error_msg = f'File size more than 1 Mb; must be less.'
+                error_msg = f'File size more than {self.MAX_FILE_SIZE} Kb; must be less.'
                 self.logger.error(error_msg)
                 response = {'status': 'failed', 'error': f'{error_msg}', 'notifications': [{'code': 4}]}
                 self.write(json.dumps(response))
             else:
-                named_as = self.svg_manager.write(_file['filename'], _file['body'])
+                named_as = self.file_manager.write(_file['filename'], _file['body'])
                 response = {'status': 'ok', 'filename': named_as, 'notifications': [{'code': 3}]}
                 self.write(json.dumps(response))
         except Exception as e:
@@ -45,7 +46,7 @@ class SvgLoadHandler(BaseHandler):
     async def delete(self):
         try:
             filename = self.get_argument('filename')
-            deleted = self.svg_manager.delete(filename)
+            deleted = self.file_manager.delete(filename)
             if deleted:
                 self.write(json.dumps({'status': 'ok'}))
             else:
